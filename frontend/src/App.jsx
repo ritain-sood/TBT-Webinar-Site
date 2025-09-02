4201ef4b8ffb26f86efd01457b2985c6e666d28b
Ritain Sood <iamritainsood.04@gmail.com>
2025-09-02 08:29:09 +0530

    chore: update CSS, index.html and App.jsx
    - Applied style updates for improved layout.
    - Modified index.html for structure.
    - Modified App.jsx for structure.

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,14 @@
 // import { useState } from 'react'
 import './App.css'
 
+import WebinarPage from './pages/WebinarPage';
+
 function App() {
 
   return (
-    <>
-      
-    </>
+    <main className=''>
+      <WebinarPage/>
+    </main>
   )
 }
 
