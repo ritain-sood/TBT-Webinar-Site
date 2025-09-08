a7a359ffd6f203d11c7a6ff6118c1bcfa2e82eb5
Ritain Sood <iamritainsood.04@gmail.com>
2025-09-08 12:28:40 +0530

    chore: Port Change

--- backend/app.js
+++ backend/app.js
@@ -22,5 +22,7 @@ app.use(bodyParser.json());
 app.use('/api', paymentRoutes);
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+const PORT = process.env.PORT || 5050;
+app.listen(PORT, () =>
+  console.log(`🚀 Server running on ${PORT}`)
+);
